db06dfce03414a65a702e330880b169159f4e34b
Billie Zhang <billiecoder@gmail.com>
2014-04-28 21:10:35 +0800

    nice

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TestBs::Application.routes.draw do
+  get "welcome/index"
+  post "open_url" => "welcome#open_url"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ TestBs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
